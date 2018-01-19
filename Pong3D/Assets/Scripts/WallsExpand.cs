31b5def5c9f48b3760140791c84ecf5733bad3cd
Zachary Vanscoit <zachary816@gmail.com>
2018-01-19 15:51:45 -0600

    Adjusted Pong3D
    started working on expanding walls.

---
+++ Pong3D/Assets/Scripts/WallsExpand.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class WallsExpand : MonoBehaviour {
+
+    public GameObject wall1, wall2;
+    public PaddleController p1, p2;
+    public float wallSpeedIncrement = 0.2f;
+    
+    private float timeIncrement = 4;
+    private readonly float maxTime = 4;
+	
+	void Update () {
+        timeIncrement -= Time.deltaTime;
+        if (timeIncrement <= 0) {
+            timeIncrement = maxTime;
+            if (wall1.transform.position.z > -50) {
+                float temp = wall1.transform.position.z;
+                temp -= wallSpeedIncrement;
+                wall1.transform.position.Set(0,0,temp);
+            }
+            if (wall2.transform.position.z < 50) {
+                float temp = wall2.transform.position.z;
+                temp += wallSpeedIncrement;
+                wall2.transform.position.Set(0, 0, temp);
+            }
+        }
+	}
+}
