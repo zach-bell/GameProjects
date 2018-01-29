0b6e2396a499289f658b1e4777d7339d84e76e9b
Zachary Vanscoit <zachary816@gmail.com>
2018-01-29 02:40:09 -0600

    Finished Pong and Adjusted pacman

---
+++ Pacman/Assets/Scripts/FollowPlayerBetter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class FollowPlayerBetter : MonoBehaviour {
+
+    [SerializeField]
+    private Transform playerPos;
+    [SerializeField]
+    private Vector3 offset;
+
+    private void Start() {
+        if (playerPos == null)
+            playerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void Update() {
+        transform.position = playerPos.position + offset;
+    }
+}
