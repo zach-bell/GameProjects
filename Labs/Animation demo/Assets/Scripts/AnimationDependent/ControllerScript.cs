cab34cb5fb1ccacd088e6ef522ae26d43d1ec1b0
Zachary Vanscoit <zachary816@gmail.com>
2018-02-11 20:42:00 -0600

    Working on Animator lab

--- Labs/Animation demo/Assets/Scripts/AnimationDependent/ControllerScript.cs
+++ Labs/Animation demo/Assets/Scripts/AnimationDependent/ControllerScript.cs
@@ -2,13 +2,41 @@
 
 public class ControllerScript : MonoBehaviour {
 
-	// Use this for initialization
+	private Animator animator;
+
 	void Start () {
-		
+		animator = GetComponent<Animator>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+		animator.SetFloat("VSpeed", Input.GetAxis("Vertical"));
+		animator.SetFloat("HSpeed", Input.GetAxis("Horizontal"));
+
+		if (Input.GetButtonDown("Jump")) {
+			animator.SetBool("Jumping", true);
+			Invoke("StopJumping", 0.1f);
+		}
+
+		if (Input.GetKey("q")) {
+			transform.Rotate(Vector3.down * Time.deltaTime * 100.0f);
+			if ((Input.GetAxis("Vertical") == 0f) && (Input.GetAxis("Horizontal") == 0f)) {
+				animator.SetBool("TurnLeft", true);
+			}
+		} else {
+			animator.SetBool("TurnLeft", false);
+		}
+		if (Input.GetKey("e")) {
+			transform.Rotate(Vector3.up * Time.deltaTime * 100.0f);
+			if ((Input.GetAxis("Vertical") == 0f) && (Input.GetAxis("Horizontal") == 0f)) {
+				animator.SetBool("TurnRight", true);
+			}
+		}
+		else {
+			animator.SetBool("TurnRight", false);
+		}
+	}
+
+	private void StopJumping() {
+		animator.SetBool("Jumping", false);
 	}
 }
---
+++ Labs/Animation demo/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public Transform PlayerT;
+
+	public bool isCamera = false;
+
+	public bool needParent = true;
+
+	public Transform parent;
+
+	public Transform lookAtPoint;
+
+	public Vector3 Offset;
+
+	void Start () {
+		if (PlayerT == null)
+			PlayerT = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+	}
+	
+	void Update () {
+		if (Input.GetKey("e"))
+			if (needParent)
+				parent.Rotate(Vector3.up * Time.deltaTime * 100.0f);
+		if (Input.GetKey("q"))
+			if (needParent)
+				parent.Rotate(Vector3.down * Time.deltaTime * 100.0f);
+		transform.position = (PlayerT.position + Offset);
+
+		if (isCamera) {
+			transform.LookAt(lookAtPoint);
+		}
+	}
+}
